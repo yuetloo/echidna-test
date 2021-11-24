ee510c5defbf7198b7c7c7d8003fbbbbd95e5ed1
yuetloo <contact@yuetloo.com>
2021-11-24 16:53:01 -0500

    import test

--- contracts/tests/EchidnaTest.sol
+++ contracts/tests/EchidnaTest.sol
@@ -3,18 +3,17 @@
  */
 
 pragma solidity ^0.8.0;
-//import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-//import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
-//contract EchidnaTest is UUPSUpgradeable, Initializable {
-contract EchidnaTest {
+contract EchidnaTest is UUPSUpgradeable, Initializable {
     function echidna_test() external pure returns (bool) {
         return true;
     }
 
-/*
+
     function _authorizeUpgrade(address) internal view override {
         require(msg.sender != address(0));
     }
-*/
+
 }
