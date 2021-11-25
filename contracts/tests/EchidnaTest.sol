881e70d858dcc2cec4a0646bc72fca146df5b9a0
yuetloo <contact@yuetloo.com>
2021-11-25 12:58:33 -0500

    add a function

--- contracts/tests/EchidnaTest.sol
+++ contracts/tests/EchidnaTest.sol
@@ -7,10 +7,13 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract EchidnaTest is UUPSUpgradeable, Initializable {
+    function doOtherStuff() external pure returns (uint8) {
+        return 9;
+    }
+
     function echidna_test() external pure returns (bool) {
         return true;
     }
-
 
     function _authorizeUpgrade(address) internal view override {
         require(msg.sender != address(0));
