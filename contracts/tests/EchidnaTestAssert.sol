0798cd9aa137c5cf48ca92054930d23178b48f1d
yuetloo <contact@yuetloo.com>
2021-12-02 21:49:54 -0500

    test again

--- contracts/tests/EchidnaTestAssert.sol
+++ contracts/tests/EchidnaTestAssert.sol
@@ -8,32 +8,12 @@ import "./TestAssert.sol";
 contract EchidnaTestAssert {
   TestAssert ta;
   event AssertionFailed(string message);
-  constructor() public {
+  constructor() {
     ta = new TestAssert();
   }
 
-  function set0(int val) public returns (bool){
-    assert(val % 100 != 0);
-  }
-
-  function internal_func(uint val) internal {
-    if(val > 128)
-      emit AssertionFailed("error");
-  }
-
-  function set1(uint val) public {}
-
-  function f() public {
-    emit AssertionFailed("error");
-    revert();
-  }
-
-  function internal_assert(uint val) public returns (bool){
-    internal_func(val);
-  }
-
-  function external_assert(uint val) public returns (bool){
-    ta.fail(val);
+  function echidna_test_address() public view returns (bool) {
+    return address(ta) != address(0);
   }
 
 }
